0992dd7d9f40f27b054e0b9ede92ab384a624388
Matthew Vargeson <mvargeson@soliantconsulting.com>
2014-08-10 10:47:33 -0700

    remove box from legend in sub metering plot

--- plot4.R
+++ plot4.R
@@ -35,7 +35,7 @@ points(x=hpc$DateTime, y=hpc$Sub_metering_2, type="l", col="red")
 points(x=hpc$DateTime, y=hpc$Sub_metering_3, type="l", col="blue")
 
 legend("topright", c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), 
-       lty=c(1, 1, 1))
+       lty=c(1, 1, 1), bty="n")
 
 # Global Reactive Power date
 plot(x=hpc$DateTime, y=hpc$Global_reactive_power, type="l", col="black", xlab="datetime",
